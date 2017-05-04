94322515aecdfa7dc025da49186864ea1cf5ba38
Gary Andelin <gary.l.andelin@boeing.com>
2017-05-04 15:27:29 -0700

    added inputs, javascript mostly working

---
+++ js/inputs.js
@@ -0,0 +1,50 @@
+var allInputs = $(':input');
+allInputs.each(function(inp) {
+  var inType = ($(this).attr('type'));
+  if (inType == undefined) {
+    console.log($(this).attr('name'))
+    $(this).val('blue');
+    setValueEntered($(this));
+  }
+  else {
+    console.log(inType);
+    var value = getValueEntered($(this));
+    if (inType == 'checkbox') {
+      $(this).prop("checked", value)
+    }
+    else {
+      $(this).val(value);
+    }
+  }
+  // create event listener
+  $(this).on("change", setValueEntered);
+});
+
+/*
+ * Returns the value entered field from the current list item.
+ */
+function getValueEntered(inp) {
+  var li = inp.parent();
+  var current = li.find('span');
+  return current.text();
+}
+
+/*
+ * Sets the Value Entered field in the current list item to the input value.
+ */
+function setValueEntered(evt) {
+  var inp = $(this);
+  var li = inp.parent();
+  var current = li.find('span');
+  if (inp.attr('type') == 'checkbox') {
+    if (inp.prop("checked")) {
+      current.text("true");
+    }
+    else {
+      current.text("false");
+    }
+  }
+  else {
+    current.text(inp.val());
+  }
+}
